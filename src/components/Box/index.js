0d1968539493bc15a28a1ce89e46c3d1080f731f
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-07-17 02:22:53 -0300

    aula 2

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,15 +1,15 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Box = styled.div`
-  background: #FFFFFF;
+  background: #ffffff;
   border-radius: 8px;
-
   padding: 16px;
-
+  
+  /* CSS previamente definido */
   margin-bottom: 10px;
   .boxLink {
     font-size: 14px;
-    color: #2E7BB4;
+    color: #2e7bb4;
     text-decoration: none;
     font-weight: 800;
   }
@@ -34,11 +34,11 @@ const Box = styled.div`
     margin-top: 12px;
     margin-bottom: 8px;
     border-color: transparent;
-    border-bottom-color: #ECF2FA;
+    border-bottom-color: #ecf2fa;
   }
   input {
-    width: 100%
-    background-color: #F4F4F4;
+    width: 100%;
+    background-color: #f4f4f4;
     color: #333333;
     border: 0;
     padding: 14px 16px;
@@ -52,9 +52,9 @@ const Box = styled.div`
   button {
     border: 0;
     padding: 8px 12px;
-    color: #FFFFFF;
+    color: #ffffff;
     border-radius: 10000px;
-    background-color: #6F92BB;
+    background-color: #6f92bb;
   }
 `;
 
