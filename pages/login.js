c99e83688a0a3b426c83ba57c08e503126e170c7
Katharine Fernandes <katharinerodrigues2004if@gmail.com>
2021-07-20 15:05:04 -0300

    aula 5

--- pages/login.js
+++ pages/login.js
@@ -73,7 +73,7 @@ export default function LoginScreen() {
 
         <footer className="footerArea">
           <p>
-            © 2021 Alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+            © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
           </p>
         </footer>
       </div>
